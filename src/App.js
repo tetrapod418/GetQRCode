4e48f06951315c6016d2443c7d79743fc5907933
tetrapod418 <hikeybow@gmail.com>
2023-09-24 09:37:55 +0900

    import QRCode visualize test

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
+import React, {useState} from "react";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [targetURL, setInputElement] = useState("");
+  const [text, setText] = useState("ここに表示されます。");
+
+  const getQRimage = () => {
+    if(targetURL !== "") {
+      // 入力されたURLをもとにAPI呼び出し文字列を生成する
+      setText(`http://api.qrserver.com/v1/create-qr-code/?data=${targetURL}&size=100x100`);
+      setInputElement("");
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <div className="url">
+            <input value={targetURL} onChange={(e) => setInputElement(e.target.value)} type="text" placeholder="URLを入力してください" />
+            <button onClick={getQRimage}>表示する</button>
+          </div>
+          <div className="print">
+          <p>{text}</p>
+          </div>
+          <div className="QRCodeImage">
+            <img src={text} alt="" title="" />
+          </div>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
