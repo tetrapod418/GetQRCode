508782f375ef0d7da626a8cd4cabe021cbbab4de
tetrapod418 <hikeybow@gmail.com>
2023-10-20 22:49:37 +0900

    appended to check need update.

---
+++ jobs/checkNeedUpdate.js
@@ -0,0 +1,35 @@
+const {KintoneRestAPIClient} = require('@kintone/rest-api-client');
+
+function getQRCodeUrl(url) {
+  return `http://api.qrserver.com/v1/create-qr-code/?data=${url}&size=100x100`;
+}
+
+(async () => {
+    try {
+      // クライアントの作成
+      const client = new KintoneRestAPIClient({
+        // kintoneのデータ取得先を設定
+        baseUrl: 'https://1lc011kswasj.cybozu.com',
+        auth: {
+          apiToken: process.env.KINTONE_API_TOKEN
+        }
+      });
+  
+      // リクエストパラメータの設定
+      const APP_ID = 2;
+      const query_string = 'ステータス="accepted" order by $id';
+      const params = {
+        app: APP_ID,
+        fields:['$id', 'ステータス', 'title', 'URL', 'descriptions'],
+        query: query_string
+      };
+      
+      // レコードの取得
+      const resp = await client.record.getRecords(params);
+      console.log('check for need update');
+      return resp !== undefined && resp.records.length > 0; 
+
+    } catch (err) {
+      console.log(err);
+    }
+  })();
