75d40d87fdd41f8d530f1412dc77a656615b8785
tetrapod418 <hikeybow@gmail.com>
2023-10-22 07:45:55 +0000

    generated

--- src/url_list.js
+++ src/url_list.js
@@ -5,6 +5,8 @@ export function UrlList() {
 <li><div>Cybozu Inside Out<div><img src="http://api.qrserver.com/v1/create-qr-code/?data=https://blog.cybozu.io/&size=100x100" alt="https://blog.cybozu.io/" title="Cybozu Inside Out" /><div>Cybozu Inside Out　は、
 サイボウズの開発本部、運用本部、サイボウズ・ラボのエンジニアのブログです。</div></div></div></li>
 
+<li><div>Cybozu Tech<div><img src="http://api.qrserver.com/v1/create-qr-code/?data=https://tech.cybozu.io&size=100x100" alt="https://tech.cybozu.io" title="Cybozu Tech" /><div>テスト用のURL登録</div></div></div></li>
+
 </ul>
 );
 }
