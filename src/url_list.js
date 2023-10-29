ca01acd72ff5bfef975753f81299689394b44444
tetrapod418 <hikeybow@gmail.com>
2023-10-29 12:16:11 +0000

    generated

--- src/url_list.js
+++ src/url_list.js
@@ -1,12 +1,11 @@
 export function UrlList() { 
 	 return ( <ul>
-<li><div>テスト資料<div><img src="http://api.qrserver.com/v1/create-qr-code/?data=https://exsample.com/&size=100x100" alt="https://exsample.com/" title="テスト資料" /><div>テスト用のデータ</div></div></div></li>
-
-<li><div>Cybozu Inside Out<div><img src="http://api.qrserver.com/v1/create-qr-code/?data=https://blog.cybozu.io/&size=100x100" alt="https://blog.cybozu.io/" title="Cybozu Inside Out" /><div>Cybozu Inside Out　は、
+		<li><div>テスト実行2023-10-29<div><img src="http://api.qrserver.com/v1/create-qr-code/?data=https://2023-10-29.example.com/&size=100x100" alt="https://2023-10-29.example.com/" title="テスト実行2023-10-29" /><div>最新版の処理のテスト用</div></div></div></li>
+		<li><div>TypeScriptに置き換え<div><img src="http://api.qrserver.com/v1/create-qr-code/?data=https://typescript.example.com/&size=100x100" alt="https://typescript.example.com/" title="TypeScriptに置き換え" /><div>JavaScriptで記述したレコード読み込み処理を、TypeScriptに置き換えてみた。</div></div></div></li>
+		<li><div>テスト資料<div><img src="http://api.qrserver.com/v1/create-qr-code/?data=https://exsample.com/&size=100x100" alt="https://exsample.com/" title="テスト資料" /><div>テスト用のデータ</div></div></div></li>
+		<li><div>Cybozu Inside Out<div><img src="http://api.qrserver.com/v1/create-qr-code/?data=https://blog.cybozu.io/&size=100x100" alt="https://blog.cybozu.io/" title="Cybozu Inside Out" /><div>Cybozu Inside Out　は、
 サイボウズの開発本部、運用本部、サイボウズ・ラボのエンジニアのブログです。</div></div></div></li>
-
-<li><div>Cybozu Tech<div><img src="http://api.qrserver.com/v1/create-qr-code/?data=https://tech.cybozu.io&size=100x100" alt="https://tech.cybozu.io" title="Cybozu Tech" /><div>テスト用のURL登録</div></div></div></li>
-
+		<li><div>Cybozu Tech<div><img src="http://api.qrserver.com/v1/create-qr-code/?data=https://tech.cybozu.io&size=100x100" alt="https://tech.cybozu.io" title="Cybozu Tech" /><div>テスト用のURL登録</div></div></div></li>
 </ul>
 );
 }
